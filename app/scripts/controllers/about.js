849dedc1cdd95538962f6b12a00d06c2d8939a7b
Deivid <deividscriollo@gmail.com>
2016-10-13 21:57:34 -0500

    actualizacion informacion
    instalacion y nueva configuracion version estable de next yeoman y sus
    dependencias

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nextbook20App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nextbook20App
+ */
+angular.module('nextbook20App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nextbook20App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nextbook20App
+ */
+angular.module('nextbook20App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
