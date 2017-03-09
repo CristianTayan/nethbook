a523f83e89d62b9d6a7f94062f3b6eb6a2c60744
Ald121 <fallsouls@hotmail.com>
2017-03-09 17:19:43 -0500

    division de inventario productos
    division de inventario productos

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,7 +24,6 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-aria/angular-aria.js',
-      'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
@@ -59,6 +58,7 @@ module.exports = function(config) {
       'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-soundmanager2/dist/angular-soundmanager2.js',
       'bower_components/angular-material-accordion/dist/angular-material-accordion.js',
+      'bower_components/angular-ui-tree/dist/angular-ui-tree.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
