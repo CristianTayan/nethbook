60e3bccdcec14d410165fef2a7d48985788674e3
Deivid <deividscriollo@gmail.com>
2017-04-21 17:35:21 -0500

    configuracion procesos iniciales

---
+++ app/scripts/services/service_url.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name nextbook20App.serviceUrl
+ * @description
+ * # serviceUrl
+ * Service in the nextbook20App.
+ */
+angular.module('nextbook20App')
+  .service('serviceUrl', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
