767fe33ddb270a7eb2e04ab2fc254d63a79f9725
Deivid <deividscriollo@gmail.com>
2017-05-23 17:18:13 -0500

    procesos de busquedas generadas

--- app/scripts/controllers/main_.js
+++ app/scripts/controllers/main_.js
@@ -7,37 +7,26 @@
  * # MainCtrlCtrl
  * Controller of the nextbook20App
  */
-angular.module('nextbook20App')
-  	.controller('main_Ctrl', function ($scope, $location, mainService, $http) {
+var app = angular.module('nextbook20App')
+  	app.controller('main_Ctrl', function ($scope, $location, mainService, $http) {
+        $scope.img = true;
         $scope.buscando = function(){
             $scope.elementos = [];
             var texto = $scope.data_search;       
             if (texto.length > 2) {
+                $scope.img = false;
                 mainService.search_empresas().get({id: texto}).$promise.then(function (res) {
                     $scope.elementos = [];
                     $scope.elementos = res.data;
                 });
             }else{
                 $scope.elementos = [];
+                $scope.img = true;
             }
         }
         $scope.selectedItemChange = function(item) {
-            console.log(item);
-            if (item) {                
-                var resultado = '';
-                if (item.nombre_comercial) {
-                    resultado = item.nombre_comercial.replace(/ /g, "_");
-                    // console.log(resultado);
-                    $location.path('/search/' + resultado);
-                }else if(item.razon_social){
-                    resultado = item.nombre_comercial.replace(/ /g, "_");
-                    // console.log(resultado);
-                    $location.path('/search/' + resultado);
-                }else{
-                    // console.log(resultado);
-                    $location.path('/search/' + item.ruc);
-                }
-
+            if (item) {
+                $location.path('/search/' + item.ruc);
             }
         }
   	})
