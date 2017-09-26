7b2dc15ed48aa2c5c0f9256f8322ae560298c328
Alex <w.figo.1991@gmail.com>
2017-09-26 17:26:53 -0500

    nb-15, creacion de cuadro de dialogo en buscador

--- app/scripts/controllers/main_.js
+++ app/scripts/controllers/main_.js
@@ -8,30 +8,43 @@
  * Controller of the nextbook20App
  */
 var app = angular.module('nextbook20App')
-  	app.controller('main_Ctrl', function ($scope, $location, mainService, $http) {
-        $scope.img = true;
-        $scope.buscando = function(){
-            $scope.elementos = [];
-            var texto = $scope.data_search;
-            if (texto) {
-                if (texto.length > 2) {
-                    $scope.img = false;
-                    mainService.search_empresas().get({id: texto}).$promise.then((res) => {
-                        $scope.elementos = [];
-                        $scope.elementos = res;
-                    });
-                }else{
-                    $scope.elementos = [];
-                    $scope.img = true;
-                }
-            }            
-        }
-        $scope.selectedItemChange = function(item) {
+app.controller('main_Ctrl', function ($scope, $location, mainService, $http , $mdDialog) {
+$scope.img = true;
+$scope.buscando = function(){
+    $scope.elementos = [];
+    var texto = $scope.data_search;
+    if (texto) {
+        if (texto.length > 2) {
+            $scope.img = false;
+            mainService.search_empresas().get({id: texto}).$promise.then((res) => {
+                $scope.elementos = [];
+                $scope.elementos = res;
+                if (texto.length > 12 && res.length == 0) {
+                    $mdDialog.show(
+                      $mdDialog.alert()
+                        .clickOutsideToClose(true)
+                       .title('LO SENTIMOS :(')
+                        .textContent('NÚMERO DE RUC NO EXISTE')
+                        .ok('ENTENDIDO')
+                        .openFrom('#left')
+                        .closeTo(angular.element(document.querySelector('#right')))
+                    );
 
-            if (item) {
-                const res = item.empresa.split(',');
-                $location.path('/search/' + res[0]);
+                  //alert('no encontrado en el sri');
+                }
+            });
+        }else{
+            $scope.elementos = [];
+            $scope.img = true;
             }
-        }
-  	})
+    }                  
+}
+$scope.selectedItemChange = function(item) {
+
+    if (item) {
+        const res = item.empresa.split(',');
+        $location.path('/search/' + res[0]);
+    }
+}
+})
 
