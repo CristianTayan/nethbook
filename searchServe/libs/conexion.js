1f0a5443faa88f97a395a9ba144c544f95b44a41
JuanKlopez <jc_lopez_81@yahoo.es>
2017-09-27 12:52:10 -0500

    select lugares

--- searchServe/libs/conexion.js
+++ searchServe/libs/conexion.js
@@ -3,7 +3,7 @@ var db;
 
 module.exports = function(){
 	if (!db) {
-		db = mongoose.connect('mongodb://localhost/dataEmpresa', {userMongoClient: true});
+		db = mongoose.connect('mongodb://localhost/dataEmpresas', {userMongoClient: true});
 	}
 	return db;
 }
