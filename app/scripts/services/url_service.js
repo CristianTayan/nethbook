cf42f6fd4278b59d1b3605e38e3aba6098bd173d
Deivid <deividscriollo@gmail.com>
2017-10-18 15:19:40 -0500

    cambio en general

--- app/scripts/services/url_service.js
+++ app/scripts/services/url_service.js
@@ -1,44 +1,32 @@
 'use strict';
-
-/**
- * @ngdoc service
- * @name nextbook20App.urlService
- * @description
- * # urlService
- * Service in the nextbook20App.
- */
 angular.module('nextbook20App')
   .service('urlService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
     this.server=function() {
+      const dominio = window.location.origin;
         return {
             appserviciosnext: function() {
-               // return "http://186.4.167.12/appserviciosnext/public/index.php/";
-               return "http://www.nethbook.com/nethbook/servicios/public/index.php/";
+              return dominio + '/nethbook/servicios/public/index.php/';
             }
             ,appnext: function() {
-                // return "http://186.4.167.12/appnext/";
-                return "http://www.nethbook.com/nethbook/server/public/index.php/";
+              return dominio + '/nethbook/server/public/index.php/';
 
             },appnextxml: function() {
-                // return "http://186.4.167.12/appnextP/";
-                //return "http://localhost:8000/";
+              return dominio + '/server/';
             },mod_radio: function() {
-                // return "http://186.4.167.12/mod_radio/";
-                return "http://192.168.0.109/mod_radio/";
+              // return "http://186.4.167.12/mod_radio/";
+              return "http://192.168.0.109/mod_radio/";
 
             },topmusical: function() {
-                // return "http://186.4.167.12/mod_radio/";
-                return "http://186.4.167.12/api-admin-oyefm/public/index.php/";
+              // return "http://186.4.167.12/mod_radio/";
+              return "http://186.4.167.12/api-admin-oyefm/public/index.php/";
             },search_empresas: function() {
-                // return "http://186.4.167.12/mod_radio/";
-                return "http://localhost:3000/empresas/";
+              // return "http://186.4.167.12/mod_radio/";
+              return "http://localhost:3000/empresas/";
 
             }
             ,dir: function() {
-                // return "http://186.4.167.12/mod_radio/";
-                return "http://localhost:8000/";
-
+              // return "http://186.4.167.12/mod_radio/";
+              return "http://localhost:8000/";
             }
         }
     };
