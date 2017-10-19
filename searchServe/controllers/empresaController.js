779e737b17e1bef9eaca698ec41f02d286d335c4
Deivid <deividscriollo@gmail.com>
2017-10-19 09:04:29 -0500

    servidor searchserver

--- searchServe/controllers/empresaController.js
+++ searchServe/controllers/empresaController.js
@@ -6,21 +6,21 @@ var empresaModel = require('../models/empresaModel.js');
  * @description :: Server-side logic for managing empresas.
  */
 module.exports = {
-    show: function (req, res) {
-        var id = req.params.id;
-        empresaModel.find({empresa: { $regex: id, $options: 'i' }}, function (err, empresa) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting empresa.',
-                    error: err
-                });
-            }
-            if (!empresa) {
-                return res.status(404).json({
-                    message: 'No such empresa'
-                });
-            }
-            return res.json(empresa);
-        }).limit(20);
-    }
+  show: function (req, res) {
+    var id = req.params.id;
+    empresaModel.find({empresa: { $regex: id, $options: 'i' }}, function (err, empresa) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Error when getting empresa.',
+                error: err
+            });
+        }
+        if (!empresa) {
+            return res.status(404).json({
+                message: 'No such empresa'
+            });
+        }
+        return res.json(empresa);
+    }).limit(20);
+  }
 };
