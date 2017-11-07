d306e825941eb84dbfc35e2d0fab904d10ff0274
juanK <jc_lopez_81@yahoo.es>
2017-11-07 17:40:59 -0500

    nb-45, cambios en el home muestra perfiles de usuario y sucursal

--- app/scripts/controllers/actualizar_datos_.js
+++ app/scripts/controllers/actualizar_datos_.js
@@ -61,7 +61,7 @@ var app = angular.module('nextbook20App');
     }
 
     $scope.cambiar_datos_password = function(){
-    	mainService.Update_Password().get({pass:$scope.data.password}).$promise.then(function(data){
+    	mainService.Update_Password().get({pass:$scope.password}).$promise.then(function(data){
         if (data.respuesta === true) {
           $location.path('/Seleccionar_Sucursal');
         }
--- app/scripts/controllers/inicio_.js
+++ app/scripts/controllers/inicio_.js
@@ -8,10 +8,13 @@
  * Controller of the nextbook20App
  */
 angular.module('nextbook20App')
-  .controller('inicio_Ctrl', function () {
+  .controller('inicio_Ctrl', function ($scope,$localStorage) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+     $scope.datos2 = $localStorage.datosE;
+     $scope.data_usuario = $localStorage.datosPersona;
+     
   });
