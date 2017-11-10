40c757894f4778eced54d159010cd65b87b9cc46
root <root@host-186-4-167-12.netlife.ec>
2017-11-10 09:38:54 -0500

    url cambios

--- app/scripts/services/url_service.js
+++ app/scripts/services/url_service.js
@@ -16,19 +16,15 @@ angular.module('nextbook20App')
           },appnextxml: function() {
             return dominio + '/server/';
           },mod_radio: function() {
-            // return "http://186.4.167.12/mod_radio/";
             return "http://192.168.0.109/mod_radio/";
 
           },topmusical: function() {
-            // return "http://186.4.167.12/mod_radio/";
             return "http://186.4.167.12/api-admin-oyefm/public/index.php/";
           },search_empresas: function() {
-            // return "http://186.4.167.12/mod_radio/";
             return dominio + '/empresas/';
           }
           ,dir: function() {
-            // return "http://186.4.167.12/mod_radio/";
-            return "http://localhost/nethbook/server/";
+            return dominio + '/nethbook/server/';
           }
       }
     };
