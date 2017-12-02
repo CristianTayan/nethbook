6c8ef5860450d15da6a52847bb8af75cf394c53f
JuanK <jc_lopez_81@yahoo.es>
2017-12-01 22:39:49 -0500

    nb-57, anotar tarea y actualizar

--- server/app/Http/Controllers/restaurarContrasenia.php
+++ server/app/Http/Controllers/restaurarContrasenia.php
@@ -13,25 +13,18 @@ use Mail;
 class restaurarContrasenia extends Controller
 {
    public function __construct(){
-      // Modelos
         $this->usuarios=new Usuarios();
         $this->ingresos_usuarios=new ingresos_usuarios();
-        // $this->actualizarClave = new actualizarClave();
     }
    
     public function recuperaClave(Request $request)
     {
         $cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
         $longitudCadena = strlen($cadena);
-        //Se define la variable que va a contener la contraseña
         $pass = "";
-        //Se define la longitud de la contraseña, en mi caso 10, pero puedes poner la longitud que quieras
         $longitudPass = 6;
-        //Creamos la contraseña
         for($i = 1 ; $i <= $longitudPass ; $i++){
-            //Definimos numero aleatorio entre 0 y la longitud de la cadena de caracteres-1
             $pos = rand(0,$longitudCadena-1);
-            //Vamos formando la contraseña en cada iteraccion del bucle, añadiendo a la cadena $pass la letra correspondiente a la posicion $pos en la cadena de caracteres definida.
             $pass .= substr($cadena,$pos,1);
         }
             
