e66982fe6b922a44829ce6604cdcfd3db2d101b7
David Criollo <deividscriollo@gmail.com>
2017-12-06 21:40:51 -0500

    70, personalizacion vista empresa

---
+++ app/scripts/controllers/perfilSucursalCtrl.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var app = angular.module('nextbook20App');
+  app.controller('perfilSucursalCtrl', function($scope, $rootScope, $location, $localStorage, urlService) {
+    $rootScope.imgPortada = urlService.server().dir() + $localStorage.imgPortada;
+    $rootScope.imgPerfil = urlService.server().dir() + $localStorage.imgPerfil;
+
+    $scope.tabSelect = (value) => {
+      $location.url('/nb/sucursal/' + value);
+    }
+  });
+
+  app.controller('infoSucursalCtrl', function($scope, $localStorage){
+    $scope.datos2 = $localStorage.datosE;
+    $scope.sucursal = $localStorage.sucursal.localizacion_sucursal.direccion;
+  });
+
+  app.controller('mapaSucursalCtrl', function($scope, $localStorage, $q){
+    
+    $scope.sucursal = $localStorage.sucursal.localizacion_sucursal.direccion;
+
+    /*global google */
+    var geocoder = new google.maps.Geocoder();
+    let direccion = $localStorage.sucursal.localizacion_sucursal.direccion;
+
+    // inicializar mapa con valores quemados
+    angular.extend($scope, { london: { lat: -1.003824, lng: -78.486328, zoom: 16 } });
+    
+    var getCordenadas = function() {
+      var deferred = $q.defer();                       
+      geocoder.geocode({
+        'address': direccion
+      }, function(results) {
+        deferred.resolve(results[0]);
+      });        
+      return deferred.promise;     
+    };
+
+    if (direccion !== '') {
+      $q.all([getCordenadas()]).then(function(res) {
+        const cordenadas = res[0].geometry.location;
+        angular.extend($scope, {
+          london: {
+            lat: cordenadas.lat(),
+            lng: cordenadas.lng(),
+            zoom: 16
+          },
+          markers: {
+            m1: {
+              lat: cordenadas.lat(),
+              lng: cordenadas.lng(),
+              focus: true,
+              message: $localStorage.datosE.razon_social + ',<br/> <small>' + direccion + '</small>',
+              icon: {
+                iconUrl: '../bower_components/leaflet/dist/images/marker-icon.png',
+                iconSize:     [20, 35],
+                iconAnchor:   [10, 10]
+              }
+            }
+          }
+        });        
+      }, function(reason) {
+        $scope.result = reason;
+      });
+    }
+  });
+
+  
--- app/scripts/services/url_service.js
+++ app/scripts/services/url_service.js
@@ -13,17 +13,13 @@ angular.module('nextbook20App') .service('urlService', function () {
             }, appnextxml: function() {
                 return dominio + '/server/';
             }, mod_radio: function() {
-                // return "http://186.4.167.12/mod_radio/";
                 return "http://192.168.0.109/mod_radio/";
             }, topmusical: function() {
-                // return "http://186.4.167.12/mod_radio/";
                 return "http://186.4.167.12/api-admin-oyefm/public/index.php/";
             }, search_empresas: function() {
-                // return "http://186.4.167.12/mod_radio/";
                 return dominio + '/empresas/';
             }, dir: function() {
-                // return "http://186.4.167.12/mod_radio/";
-                return dominio + '/nethbook/server/';
+                return dominio + '/nethbook/server';
             }
         };
     }
